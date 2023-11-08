7fefbd441d6301d974084eb2200cee15d7e4a084
Rodrigo Stuchi <rod.stuchi@gmail.com>
2023-11-08 16:55:23 -0300

    add alias pipewire in top
    fix fzf_select, update fzf_search commands in ranger

--- ranger/.config/ranger/commands.py
+++ ranger/.config/ranger/commands.py
@@ -7,7 +7,7 @@
 # A simple command for demonstration purposes follows.
 # -----------------------------------------------------------------------------
 
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
 
 import os
 import re
@@ -16,6 +16,7 @@ from collections import deque
 from ranger.api.commands import Command
 from ranger.container.file import File
 from ranger.ext.get_executables import get_executables
+
 
 # Any class that is a subclass of "Command" will be integrated into ranger as a
 # command.  Try typing ":my_edit<ENTER>" in ranger!
@@ -79,22 +80,23 @@ class mkcd(Command):
         if not lexists(dirname):
             makedirs(dirname)
 
-            match = re.search('^/|^~[^/]*/', dirname)
+            match = re.search("^/|^~[^/]*/", dirname)
             if match:
                 self.fm.cd(match.group(0))
-                dirname = dirname[match.end(0):]
+                dirname = dirname[match.end(0) :]
 
-            for m in re.finditer('[^/]+', dirname):
+            for m in re.finditer("[^/]+", dirname):
                 s = m.group(0)
-                if s == '..' or (s.startswith('.') and not self.fm.settings['show_hidden']):
+                if s == ".." or (
+                    s.startswith(".") and not self.fm.settings["show_hidden"]
+                ):
                     self.fm.cd(s)
                 else:
                     ## We force ranger to load content before calling `scout`.
                     self.fm.thisdir.load_content(schedule=False)
-                    self.fm.execute_console('scout -ae ^{}$'.format(s))
+                    self.fm.execute_console("scout -ae ^{}$".format(s))
         else:
             self.fm.notify("file/directory exists!", bad=True)
-
 
 
 class fzf_select(Command):
@@ -111,44 +113,33 @@ class fzf_select(Command):
         import os
         from ranger.ext.get_executables import get_executables
 
-        if 'fzf' not in get_executables():
-            self.fm.notify('Could not find fzf in the PATH.', bad=True)
-            return
-
-        fd = None
-        if 'fdfind' in get_executables():
-            fd = 'fdfind'
-        elif 'fd' in get_executables():
-            fd = 'fd'
-
-        if fd is not None:
-            hidden = ('--hidden' if self.fm.settings.show_hidden else '')
-            exclude = "--no-ignore-vcs --exclude '.git' --exclude '*.py[co]' --exclude '__pycache__'"
-            only_directories = ('--type directory' if self.quantifier else '')
-            fzf_default_command = '{} --follow {} {} {} --color=always'.format(
-                fd, hidden, exclude, only_directories
-            )
-        else:
-            hidden = ('-false' if self.fm.settings.show_hidden else r"-path '*/\.*' -prune")
-            exclude = r"\( -name '\.git' -o -iname '\.*py[co]' -o -fstype 'dev' -o -fstype 'proc' \) -prune"
-            only_directories = ('-type d' if self.quantifier else '')
-            fzf_default_command = 'find -L . -mindepth 1 {} -o {} -o {} -print | cut -b3-'.format(
-                hidden, exclude, only_directories
-            )
+        hidden = "--hidden" if self.fm.settings.show_hidden else ""
+        exclude = "--no-ignore-vcs --exclude '.git' --exclude '*.py[co]' --exclude '__pycache__' --exclude 'node_modules'"
+        only_directories = "--type directory" if not self.quantifier else ""
+        fzfcmd = "fd --follow {} {} {} --color=never".format(
+            hidden, exclude, only_directories
+        )
 
         env = os.environ.copy()
-        env['FZF_DEFAULT_COMMAND'] = fzf_default_command
-        env['FZF_DEFAULT_OPTS'] = '--height=40% --layout=reverse --ansi --preview="{}"'.format('''
+        env[
+            "FZF_DEFAULT_OPTS"
+        ] = '--height=40% --layout=reverse --ansi --preview="{}"'.format(
+            """
             (
                 batcat --color=always {} ||
                 bat --color=always {} ||
                 cat {} ||
                 tree -ahpCL 3 -I '.git' -I '*.py[co]' -I '__pycache__' {}
             ) 2>/dev/null | head -n 100
-        ''')
+        """
+        )
 
-        fzf = self.fm.execute_command('fzf --no-multi', env=env,
-                                      universal_newlines=True, stdout=subprocess.PIPE)
+        fzf = self.fm.execute_command(
+            fzfcmd + "| fzf --no-multi",
+            env=env,
+            universal_newlines=True,
+            stdout=subprocess.PIPE,
+        )
         stdout, _ = fzf.communicate()
         if fzf.returncode == 0:
             selected = os.path.abspath(stdout.strip())
@@ -158,44 +149,46 @@ class fzf_select(Command):
                 self.fm.select_file(selected)
 
 
-
 class YankContent(Command):
     """
     Copy the content of image file and text file with xclip
     """
 
     def execute(self):
-        if 'xclip' not in get_executables():
-            self.fm.notify('xclip is not found.', bad=True)
+        if "xclip" not in get_executables():
+            self.fm.notify("xclip is not found.", bad=True)
             return
 
         arg = self.rest(1)
         if arg:
             if not os.path.isfile(arg):
-                self.fm.notify('{} is not a file.'.format(arg))
+                self.fm.notify("{} is not a file.".format(arg))
                 return
             file = File(arg)
         else:
             file = self.fm.thisfile
             if not file.is_file:
-                self.fm.notify('{} is not a file.'.format(file.relative_path))
+                self.fm.notify("{} is not a file.".format(file.relative_path))
                 return
 
         relative_path = file.relative_path
-        cmd = ['xclip', '-selection', 'clipboard']
+        cmd = ["xclip", "-selection", "clipboard"]
         if not file.is_binary():
-            with open(file.path, 'rb') as fd:
+            with open(file.path, "rb") as fd:
                 subprocess.check_call(cmd, stdin=fd)
         elif file.image:
-            cmd += ['-t', file.mimetype, file.path]
+            cmd += ["-t", file.mimetype, file.path]
             subprocess.check_call(cmd)
-            self.fm.notify('Content of {} is copied to x clipboard'.format(relative_path))
+            self.fm.notify(
+                "Content of {} is copied to x clipboard".format(relative_path)
+            )
         else:
-            self.fm.notify('{} is not an image file or a text file.'.format(relative_path))
+            self.fm.notify(
+                "{} is not an image file or a text file.".format(relative_path)
+            )
 
     def tab(self, tabnum):
         return self._tab_directory_content()
-
 
 
 class fd_search(Command):
@@ -211,48 +204,64 @@ class fd_search(Command):
     SEARCH_RESULTS = deque()
 
     def execute(self):
+        import re
+        import subprocess
+        from ranger.ext.get_executables import get_executables
+
         self.SEARCH_RESULTS.clear()
 
-        if 'fdfind' in get_executables():
-            fd = 'fdfind'
-        elif 'fd' in get_executables():
-            fd = 'fd'
+        if "fdfind" in get_executables():
+            fd = "fdfind"
+        elif "fd" in get_executables():
+            fd = "fd"
         else:
             self.fm.notify("Couldn't find fd in the PATH.", bad=True)
             return
 
         if self.arg(1):
-            if self.arg(1)[:2] == '-d':
+            if self.arg(1)[:2] == "-d":
                 depth = self.arg(1)
                 target = self.rest(2)
             else:
-                depth = '-d1'
+                depth = "-d1"
                 target = self.rest(1)
         else:
             self.fm.notify(":fd_search needs a query.", bad=True)
             return
 
-        hidden = ('--hidden' if self.fm.settings.show_hidden else '')
-        exclude = "--no-ignore-vcs --exclude '.git' --exclude '*.py[co]' --exclude '__pycache__'"
-        command = '{} --follow {} {} {} --print0 {}'.format(
+        hidden = "--hidden" if self.fm.settings.show_hidden else ""
+        exclude = "--no-ignore-vcs --exclude '.git' --exclude '*.py[co]' --exclude '__pycache__' --exclude 'node_modules'"
+        command = "{} --follow {} {} {} --print0 {}".format(
             fd, depth, hidden, exclude, target
         )
-        fd = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        fd = self.fm.execute_command(
+            command, universal_newlines=True, stdout=subprocess.PIPE
+        )
         stdout, _ = fd.communicate()
 
         if fd.returncode == 0:
-            results = filter(None, stdout.split('\0'))
+            results = filter(None, stdout.split("\0"))
             if not self.fm.settings.show_hidden and self.fm.settings.hidden_filter:
                 hidden_filter = re.compile(self.fm.settings.hidden_filter)
-                results = filter(lambda res: not hidden_filter.search(os.path.basename(res)), results)
-            results = map(lambda res: os.path.abspath(os.path.join(self.fm.thisdir.path, res)), results)
+                results = filter(
+                    lambda res: not hidden_filter.search(os.path.basename(res)), results
+                )
+            results = map(
+                lambda res: os.path.abspath(os.path.join(self.fm.thisdir.path, res)),
+                results,
+            )
             self.SEARCH_RESULTS.extend(sorted(results, key=str.lower))
             if len(self.SEARCH_RESULTS) > 0:
-                self.fm.notify('Found {} result{}.'.format(len(self.SEARCH_RESULTS),
-                                                           ('s' if len(self.SEARCH_RESULTS) > 1 else '')))
+                self.fm.notify(
+                    "Found {} result{}.".format(
+                        len(self.SEARCH_RESULTS),
+                        ("s" if len(self.SEARCH_RESULTS) > 1 else ""),
+                    )
+                )
                 self.fm.select_file(self.SEARCH_RESULTS[0])
             else:
-                self.fm.notify('No results found.')
+                self.fm.notify("No results found.")
+
 
 class fd_next(Command):
     """
@@ -266,6 +275,7 @@ class fd_next(Command):
             self.fm.select_file(fd_search.SEARCH_RESULTS[0])
         elif len(fd_search.SEARCH_RESULTS) == 1:
             self.fm.select_file(fd_search.SEARCH_RESULTS[0])
+
 
 class fd_prev(Command):
     """
