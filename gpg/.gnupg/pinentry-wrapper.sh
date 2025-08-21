cf27433b7d63b23085b621996cb55047d7d93084
Rodrigo Stuchi <rod.stuchi@gmail.com>
2025-08-21 09:55:53 -0300

    add warning messages in pinentry-wrapper.sh if not installed

--- gpg/.gnupg/pinentry-wrapper.sh
+++ gpg/.gnupg/pinentry-wrapper.sh
@@ -10,6 +10,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     elif command -v pinentry-curses >/dev/null 2>&1; then
         exec pinentry-curses "$@"
     else
+        echo "Warning: pinentry-curses not found on macOS!" >&2
+        echo "You can install it with: brew install pinentry" >&2
         exec /usr/bin/pinentry "$@"
     fi
 else
@@ -19,6 +21,9 @@ else
     elif command -v pinentry-curses >/dev/null 2>&1; then
         exec pinentry-curses "$@"
     else
+        echo "Warning: pinentry-curses not found on Linux!" >&2
+        echo "You can install it with: sudo pacman -S pinentry (Arch Linux)" >&2
+        echo "Or use your distribution's package manager to install pinentry" >&2
         exec /usr/bin/pinentry "$@"
     fi
 fi
